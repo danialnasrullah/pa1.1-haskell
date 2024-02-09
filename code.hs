3bf8a812b9799655db30a95f934f8b6c00a91fe7
danialnasrullah <115585384+danialnasrullah@users.noreply.github.com>
2024-02-09 12:09:02 +0500

    Update code.hs

--- code.hs
+++ code.hs
@@ -1,4 +1,3 @@
-
 import Test.Hspec 
 
 -- Use the following data types for the questions below
@@ -6,27 +5,195 @@ data Tree a = Nil | TreeNode (Tree a) a (Tree a) deriving (Show, Eq)
 
 data LinkedList a = Null | ListNode a (LinkedList a) deriving (Show, Eq) 
 
+-- HELPER FUNCTIONS AND DEFINTIONS
+
+findSubPair :: Int -> [Int] -> Int -> [[Int]]
+findSubPair _ [] _ = []
+findSubPair x (y:ys) target
+    | sumEqualsTarget && xGreaterOrEqual = [pairMachine x y] `listFixer` findSubPair x ys target
+    | sumEqualsTarget && not xGreaterOrEqual = [pairMachine y x] `listFixer` findSubPair x ys target
+    | otherwise = findSubPair x ys target
+  where
+    sumEqualsTarget = x + y == target
+    xGreaterOrEqual = x >= y
+
+-- Helper function to creater pair
+pairMachine :: Int -> Int -> [Int]
+pairMachine a b = [a, b]
+
+-- append function for lists
+listFixer :: [[Int]] -> [[Int]] -> [[Int]]
+listFixer [] ys = ys
+listFixer (x:xs) ys = x : listFixer xs ys
+
+insertionSort :: Ord a => [a] -> [a]
+insertionSort [] = []
+insertionSort (x:xs) = insert x (insertionSort xs)
+  where
+    insert y [] = [y]
+    insert y (z:zs)
+      | y <= z    = y : z : zs
+      | otherwise = z : insert y zs
+
+removeDuplicates :: Eq a => [[a]] -> [[a]]
+removeDuplicates [] = []
+removeDuplicates [x] = [x]
+removeDuplicates (x:y:xs)
+  | x == y    = removeDuplicates (x:xs)
+  | otherwise = x : removeDuplicates (y:xs)
+
+findPairs :: Int -> [Int] -> [[Int]]
+findPairs target nums = removeDuplicates $ sortPairs [ [x, y] | x <- nums, y <- nums, x < y, x + y == target ]
+
+
+
+isMirror :: Eq a => Tree a -> Tree a -> Bool
+isMirror Nil Nil = True
+isMirror (TreeNode left1 val1 right1) (TreeNode left2 val2 right2) = val1 == val2 && isMirror left1 right2 && isMirror right1 left2
+isMirror _ _ = False
+
+
+reverserFunct :: [[a]] -> [a]
+reverserFunct xs = revHelper xs True
+  where
+    revHelper [] _ = []
+    revHelper (x:xs) flag
+      | flag = x ++ revHelper xs (not flag)
+      | otherwise = reverse x ++ revHelper xs (not flag)
+
+collectLevels :: [Tree a] -> [[a]]
+collectLevels [] = []
+collectLevels nodes = currentLevel : collectLevels (mergeAll nextLevels)
+  where
+    currentLevel = [x | TreeNode _ x _ <- nodes]
+    nextLevels = [(left, right) | TreeNode left _ right <- nodes]
+
+    mergeAll :: [(Tree a, Tree a)] -> [Tree a]
+    mergeAll [] = []
+    mergeAll ((left, right):rest) = left : right : mergeAll rest
+
+
+
+
+reverseList :: LinkedList a -> LinkedList a
+reverseList = reverseListHelper Null
+  where
+    reverseListHelper acc Null = acc
+    reverseListHelper acc (ListNode x xs) = reverseListHelper (ListNode x acc) xs
+
+------------------------------------------------------------------------------------------------
+linkedListToString :: LinkedList Char -> String
+linkedListToString Null = ""
+linkedListToString (ListNode c rest) = c : linkedListToString rest
+
+stringToLinkedList :: String -> LinkedList Char
+stringToLinkedList "" = Null
+stringToLinkedList (c:cs) = ListNode c (stringToLinkedList cs)
+
+-- Helper function to find the longest common prefix of two strings
+longestCommonPrefix :: String -> String -> String
+longestCommonPrefix (x:xs) (y:ys) = if x == y then x : longestCommonPrefix xs ys else []
+longestCommonPrefix _ _ = []
+
+-- Updated function to find the longest common substring
+longestCommonSubstr :: String -> String -> String
+longestCommonSubstr s1 s2 =
+    let substrings1 = [drop i s1 | i <- [0..length s1]]
+        substrings2 = [drop j s2 | j <- [0..length s2]]
+        commonSubstrings = [longestCommonPrefix s1' s2' | s1' <- substrings1, s2' <- substrings2]
+    in findLongest commonSubstrings ""
+    where
+      findLongest [] longest = longest
+      findLongest (x:xs) longest = findLongest xs (if length x > length longest then x else longest)
+
+      longestCommonPrefix (x:xs) (y:ys) | x == y = x : longestCommonPrefix xs ys
+      longestCommonPrefix _ _ = ""
+
+-----------------------------------------------------------------------------------------------------
+
+
+constructTree :: String -> Tree Char -> (Tree Char, String)
+constructTree [] tree = (tree, [])
+constructTree (x:xs) Nil
+  | x == '^' = (Nil, xs)
+  | otherwise = constructTree xs (TreeNode Nil x Nil)
+constructTree str@(x:xs) tree@(TreeNode left val right)
+  | x == '^' = case right of
+                 Nil -> let (newRight, restStr) = constructTree xs Nil 
+                        in (TreeNode left val newRight, restStr)
+                 _ -> let (parentUpdated, restStr) = constructTree xs tree 
+                      in (parentUpdated, restStr)
+  | otherwise = let (newLeft, restStr) = insertLeft str left 
+                in (TreeNode newLeft val right, restStr)
+
+insertLeft :: String -> Tree Char -> (Tree Char, String)
+insertLeft [] tree = (tree, [])
+insertLeft (x:xs) Nil
+  | x == '^' = (Nil, xs)
+  | otherwise = insertLeft xs (TreeNode Nil x Nil)
+insertLeft str@(x:xs) tree@(TreeNode left val right)
+  | x == '^' = (tree, xs)
+  | otherwise = let (newLeft, restStr) = insertLeft str left
+                in (TreeNode newLeft val right, restStr)
+--maze question
+mazeHelper :: Int -> Int -> Int -> Int -> [[Int]] -> Int
+mazeHelper x y xaxis yaxis m
+  | x == xaxis - 1 && y == yaxis - 1 = m !! x !! y
+  | x == xaxis - 1 = m !! x !! y + mazeHelper x (y + 1) xaxis yaxis m
+  | y == yaxis - 1 = m !! x !! y + mazeHelper (x + 1) y xaxis yaxis m
+  | otherwise = m !! x !! y + myMin (mazeHelper (x + 1) y xaxis yaxis m) (mazeHelper x (y + 1) xaxis yaxis m)
+    where
+      myMin a b = if a < b then a else b
+
+
 --Category: Easy
 
 -- Question 1
 targetSum :: [Int] -> Int ->[[Int]]
-targetSum = undefined
+compareLists [] y = False
+compareLists x [] = True
+compareLists (a:as) (b:bs)
+    | a > b = False
+    | a < b = True
+    | otherwise = True
+
+sortPairs [] = []
+sortPairs [x] = [x]
+sortPairs (x:y:xs)
+    | compareLists x y = x : sortPairs (y:xs)
+    | otherwise = y : sortPairs (x:xs)
+
+targetSum [] target = []
+targetSum [x] target = []
+targetSum (x:xs) target = sortPairs(findSubPair x xs target ++ targetSum xs target)
 
 -- Question 2
 symmetricTree :: Eq a => Tree a -> Bool
-symmetricTree = undefined
+symmetricTree Nil = True
+symmetricTree (TreeNode left val right) = isMirror left right
+
 
 -- Question 3
 palindromList :: Eq a => LinkedList a -> Bool
-palindromList = undefined
+appendToEnd value Null = ListNode value Null
+appendToEnd value (ListNode y ys) = ListNode y (appendToEnd value ys)
+
+reversedList Null = Null
+reversedList (ListNode x xs) = appendToEnd x (reversedList xs)
+
+palindromList Null = True
+palindromList list = list == reversedList list
+
+
 
 -- Question 4
 snakeTraversal :: Tree a -> [a]
-snakeTraversal = undefined
+snakeTraversal Nil = []
+snakeTraversal root = reverserFunct (collectLevels [root])
 
 -- Question 5
 treeConstruction :: String -> Tree Char
-treeConstruction = undefined
+treeConstruction str = fst $ constructTree str Nil
 
 
 -- Category: Medium
@@ -45,23 +212,59 @@ instance Num (Tree Int) where
 -- Question 1.2
 
 longestCommonString :: LinkedList Char -> LinkedList Char -> LinkedList Char
-longestCommonString = undefined
+longestCommonString ll1 ll2 = stringToLinkedList (longestCommonSubstr (linkedListToString ll1) (linkedListToString ll2))
+
 
 -- Question 2
 commonAncestor :: Ord a => Eq a => Tree a -> a -> a -> Maybe a
-commonAncestor = undefined
+commonAncestor Nil _ _ = Nothing
+commonAncestor (TreeNode left val right) a b
+  | val == a || val == b = Just val
+  | otherwise = case (commonAncestor left a b, commonAncestor right a b) of
+    (Just x, Just y) -> Just val
+    (Just x, Nothing) -> Just x
+    (Nothing, Just y) -> Just y
+    (Nothing, Nothing) -> Nothing
 
 -- Question 3
 gameofLife :: [[Int]] -> [[Int]]
-gameofLife = undefined
+gameofLife board
+  | null board || null (board !! 0) = [] 
+  | otherwise = [[nextState (x, y) | y <- [0..m-1]] | x <- [0..n-1]]
+  where
+    n = length board -- Number of rows
+    m = if not (null board) then length (board !! 0) else 0 
+    nextState (x, y) = case (getCell x y, liveNeighbors x y) of
+      (1, neighbors) | neighbors < 2 -> 0 -- rule1
+                     | neighbors == 2 || neighbors == 3 -> 1 --rule2
+                     | neighbors > 3 -> 0 --rule3
+      (0, 3) -> 1 --rule4
+      (_, _) -> getCell x y
+    getCell x y = if x >= 0 && x < n && y >= 0 && y < m then board !! x !! y else 0
+
+    liveNeighbors x y = countLiveNeighbors x y [-1..1] [-1..1] 0
+    countLiveNeighbors _ _ [] _ acc = acc
+    countLiveNeighbors x y (_:dxs) [] acc = countLiveNeighbors x y dxs [-1..1] acc
+    countLiveNeighbors x y (dx:dxs) (dy:dys) acc
+      | dx == 0 && dy == 0 = countLiveNeighbors x y (dx:dxs) dys acc -
+      | otherwise = countLiveNeighbors x y (dx:dxs) dys (acc + getCell (x + dx) (y + dy))
+
+
 
 -- Question 4
 waterCollection :: [Int] -> Int
-waterCollection = undefined
+waterCollection heights = sum [max 0 (min (leftMax !! i) (rightMax !! i) - heights !! i) | i <- [0..length heights - 1]]
+  where
+    leftMax = scanl max 0 heights 
+    rightMax = scanr max 0 heights 
+
 
 -- Question 5
 minPathMaze :: [[Int]] -> Int
-minPathMaze = undefined
+minPathMaze m = mazeHelper 0 0 (length m) (length (m !! 0)) m
+
+
+
 
 
 
